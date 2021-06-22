6c63b1992c1c52623047501a27a17d7e44207df4
Makssshow <makssshowwork@gmail.com>
2021-06-22 19:07:42 +0200

    a

--- assets/js/portfolio/scroll-animation.js
+++ assets/js/portfolio/scroll-animation.js
@@ -1,46 +1,46 @@
-
-  $('.a').imagesLoaded().done( function( instance ) {
-
-
+jQuery(document).ready(function ($) {
   gsap.registerPlugin(ScrollTrigger);
 
   ScrollTrigger.defaults({
     scrub: 0.05,
   });
 
-  ///////////////////////////////////HERO
-  //Hero background pin
-  ScrollTrigger.create({
-    trigger: "html",
-    start: "top top",
-    end: "bottom bottom",
-    endTrigger: ".hero",
-    pin: ".main",
-    pinSpacing: false,
-    markers: false,
-  });
-
-  //hero about pin
-  ScrollTrigger.create({
-    trigger: ".hero__title",
-    start: "top top",
-    end: "top center",
-    endTrigger: ".hero__wrap",
-    pin: true,
-    pinSpacing: false,
-    markers: false,
-  });
-
-  //hero title moves up
-  gsap.to(".hero__title", {
-    yPercent: -50,
-    scrollTrigger: {
-      trigger: ".hero__wrap",
-      start: "top center",
-      end: "center center",
+  if ($(window).width() > 1050) {
+    ///////////////////////////////////HERO
+    //Hero background pin
+    ScrollTrigger.create({
+      trigger: "html",
+      start: "top top",
+      end: "bottom bottom",
+      endTrigger: ".hero",
+      pin: ".main",
+      pinSpacing: false,
       markers: false,
-    },
-  });
+    });
+
+    //hero about pin
+    ScrollTrigger.create({
+      trigger: ".hero__title",
+      start: "top top",
+      end: "top center",
+      endTrigger: ".hero__wrap",
+      pin: true,
+      pinSpacing: false,
+      markers: false,
+    });
+
+    //hero title moves up
+    gsap.to(".hero__title", {
+      yPercent: -50,
+      scrollTrigger: {
+        trigger: ".hero__wrap",
+        start: "top center",
+        end: "center center",
+        markers: false,
+      },
+    });
+  } else {
+  }
 
   //ANIM RIGHT
   var move_right = gsap.utils.toArray(".anim_right");
@@ -53,20 +53,19 @@
         end: "center center",
         markers: false,
       },
-    })
+    });
   });
 
-    //ANIM RIGHT
-      gsap.from(".tablet img", {
-        xPercent: -20,
-        scrollTrigger: {
-          trigger: ".tablet",
-          start: "top bottom",
-          end: "top top",
-          markers: false,
-        },
-      })
-  
+  //ANIM RIGHT
+  gsap.from(".tablet img", {
+    xPercent: -20,
+    scrollTrigger: {
+      trigger: ".tablet",
+      start: "top bottom",
+      end: "bottom top",
+      markers: false,
+    },
+  });
 
   //ANIM COLORS
   gsap.from(".col-typ__title img", {
@@ -75,21 +74,18 @@
       trigger: ".col-typ__title",
       start: "top 70%",
       end: "bottom 70%",
-      markers: false
-    }
+      markers: false,
+    },
   });
 
+  //PIN GREEN BORDER
   ScrollTrigger.create({
     trigger: ".pin__background",
-    start: "center center",
+    start: "top top",
     endTrigger: ".pin img:last-child",
-    end: "center center",
+    end: "bottom bottom",
     pin: true,
     pinSpacing: false,
-    markers: false
-  })
-
-
+    markers: true,
+  });
 });
-
-
